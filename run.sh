7aedb3717748380966dbec1cfbd0bfa32c00d38d
Daniel Fonseca Alves <dalraf@gmail.com>
2016-07-17 15:26:49 -0300

    Adicionado diretorio de plugin

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f minenukkit
-docker run -it -p 19132:19132/udp -p 19132:19132/tcp --name minenukkit dalraf/nukkit
+docker run -it -v /opt/plugin:/plugin -p 19132:19132/udp -p 19132:19132/tcp --name minenukkit dalraf/nukkit
