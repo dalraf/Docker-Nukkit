1782b4ba61358343fbc2a0754133301499e9ac3c
Daniel Fonseca Alves <dalraf@gmail.com>
2016-07-18 18:26:56 -0300

    Correction

--- pluginsmake/makeplugins.sh
+++ pluginsmake/makeplugins.sh
@@ -5,7 +5,7 @@ mvn package
 cd target
 cp *jar /plugins
 cd /
-git clone https://github.com/SW-Team/EntityManager
+git clone https://github.com/SW-Team/EntityManager.git
 cd /EntityManager
 mvn package
 cd target
